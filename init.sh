07cedd328fc16fcc0ea735a7e6a6c3cb6dd56960
sato <sato@tsumikiinc.com>
2015-03-27 19:41:50 +0900

    fix

--- init.sh
+++ init.sh
@@ -3,4 +3,4 @@
 npm i -D browserify coffee-reactify live-server uglify-js watchify
 npm i -S react
 mkdir -p public/js && echo '<!doctype html>' > public/index.html
-rm -rf init.sh
+rm -rf .git init.sh
